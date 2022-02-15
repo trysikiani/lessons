0be32b88ed652b9b105c926aac2ef5a5be8a99b2
AVE MANSUROV <87867406+trysikiani@users.noreply.github.com>
2022-02-15 17:35:38 +0300

    Add files via upload

---
+++ yrok1.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class yrok1 {
+    public static void main(String[] args) {
+
+        int [] reverse = {-1,2,-3,4,-5,6,-7,8,-9,10};
+        for (int i = reverse.length - 1; i >= 0; i --) {
+            System.out.print(reverse[i] + " ");
+        }
+        }
+    }
+
+
+
+
